0540a6b59246ae54d46bc9263b1c364d32601a20
gabays <gabay.simon@gmail.com>
2021-12-10 17:53:05 +0100

    add submission scripts

---
+++ submission_scripts/submission-seg.sh
@@ -0,0 +1,15 @@
+#!/bin/env bash
+#SBATCH --partition=shared-gpu
+#SBATCH --time=01:00:00
+#SBATCH --gpus=pascal:1
+#SBATCH --output=kraken-%j.out
+#SBATCH --mem=0
+
+module load GCCcore/10.2.0 Python/3.8.6
+source ~/kraken-env/bin/activate
+
+OUTPUT_NAME="output_name"
+XML_FOLDER="/home/gabays/data"
+
+echo "KETOS training"
+srun ketos segtrain -o $OUTPUT_NAME -f alto -d cuda "${XML_FOLDER}/*.xml"
